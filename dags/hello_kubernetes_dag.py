6784bd886454ffc165e382e13c2a358091bd05e4
Samuel Viana da Silva <samuelviana2626@gmail.com>
2025-09-14 17:06:06 -0400

    adding dag example pod operator

--- dags/hello_kubernetes_dag.py
+++ dags/hello_kubernetes_dag.py
@@ -1,21 +1,21 @@
 from __future__ import annotations
 import pendulum
-from airflow.models.dag import DAG
+from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 with DAG(
-        dag_id="test_kubernetes_simple",
-        start_date=pendulum.datetime(2025, 9, 14, tz="UTC"),
-        catchup=False,
-        schedule=None,
+    dag_id="kpo_minimo",
+    start_date=pendulum.datetime(2025, 9, 14, tz="UTC"),
+    schedule=None,
+    catchup=False,
 ) as dag:
-    test_task = KubernetesPodOperator(
-        task_id="test_simple",
+    KubernetesPodOperator(
+        task_id="echo_kpo",
         namespace="airflow",
         service_account_name="airflow",
         image="bash:5.2",
-        cmds=["/bin/bash","-c"],
-        arguments=["echo Hello Kubernetes!"],
+        cmds=["/bin/bash", "-c"],
+        arguments=["echo Hello Kubernetes from KPO!"],
         get_logs=True,
         is_delete_operator_pod=True,
         in_cluster=True,
