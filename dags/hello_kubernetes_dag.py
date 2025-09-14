d28a3ae8b782b0df4636e58c939709ae4c0cc64e
Samuel Viana da Silva <samuelviana2626@gmail.com>
2025-09-14 13:15:54 -0400

    adding dag example pod operator

---
+++ dags/hello_kubernetes_dag.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+with DAG(
+    dag_id="hello_kubernetes_world",
+    start_date=pendulum.datetime(2025, 9, 14, tz="UTC"),
+    catchup=False,
+    schedule=None,
+    tags=["kubernetes", "example"],
+) as dag:
+    start = EmptyOperator(task_id="start")
+
+    hello_kubernetes_task = KubernetesPodOperator(
+        task_id="hello_kubernetes_pod_task",
+        name="pod-hello-world",
+        namespace="airflow",
+        image="bash:latest",
+        cmds=["bash", "-c"],
+        arguments=[
+            "echo '=========================================='",
+            "echo 'Olá, Mundo, a partir de um Pod Kubernetes! (Airflow 3)'",
+            "echo 'Esta tarefa foi acionada pelo Airflow.'",
+            "echo 'Aguardando 5 segundos...'",
+            "sleep 5",
+            "echo 'Tarefa concluída com sucesso!'",
+            "echo '=========================================='"
+        ],
+        do_xcom_push=False,
+    )
+
+    end = EmptyOperator(task_id="end")
+
+    start >> hello_kubernetes_task >> end
