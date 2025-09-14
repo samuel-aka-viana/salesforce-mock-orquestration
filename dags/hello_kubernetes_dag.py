4976a60a7ef8ebfe7714ed550e80f87e7910847c
Samuel Viana da Silva <samuelviana2626@gmail.com>
2025-09-14 14:15:20 -0400

    adding dag example pod operator

--- dags/hello_kubernetes_dag.py
+++ dags/hello_kubernetes_dag.py
@@ -1,20 +1,18 @@
 from __future__ import annotations
 
 import pendulum
-
 from airflow.models.dag import DAG
-from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="hello_kubernetes_world",
-    start_date=pendulum.datetime(2025, 9, 14, tz="UTC"),
-    catchup=False,
-    schedule=None,
-    tags=["kubernetes", "example"],
+        dag_id="hello_kubernetes_world",
+        start_date=pendulum.datetime(2025, 9, 14, tz="UTC"),
+        catchup=False,
+        schedule=None,
+        tags=["kubernetes", "example"],
 ) as dag:
     start = EmptyOperator(task_id="start")
-
 
     hello_kubernetes_task = KubernetesPodOperator(
         task_id="hello_kubernetes_pod_task",
@@ -33,7 +31,6 @@ with DAG(
         ],
         do_xcom_push=False,
     )
-
 
     end = EmptyOperator(task_id="end")
 
